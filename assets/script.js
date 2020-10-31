906d94d12da81afe3ff8c2c6e2de5448f8b8062e
Dan <wattierdan@live.com>
2020-10-31 08:10:33 -0700

    Refactors javaScript file

--- assets/script.js
+++ assets/script.js
@@ -12,6 +12,54 @@ var timeBlocks = $('.time_block')
 var fade = $('.fade')
 var textInput =$('.text_input')
 
+//scrolls to current time_block hour
+//fix where it scrolls to
+$(document).ready(function () {
+  $('html').animate({
+      scrollTop: $('.scroll').offset().top - 300
+  }, 1200);
+});
+
+//save button
+$('button').on("click", function(){
+  var text = $(this).siblings("textarea").val()
+  var id = $(this).parent().attr('id')
+  var aToDo = new Object()
+  aToDo.id = id
+  aToDo.text = text
+  toDos.push(aToDo)
+  saveText()
+})
+
+//saves to local storage 
+function saveText() {
+  var str = JSON.stringify(toDos)
+  localStorage.setItem("textContent", str)
+}
+
+//gets textContent from local storage 
+function getToDos() {
+ var str = localStorage.getItem('textContent')
+ toDos = JSON.parse(str)
+ if (!toDos) {
+   toDos = []
+ }
+}
+
+//function to determine what color a time_block should be
+function whatColor(){
+  for (i = 0; i < timeBlocks.length; i++) {
+    //console.log(timeBlocks[i].id, curentHour)
+    if (parseInt(timeBlocks[i].id) == curentHour) {
+      document.getElementById([i]).style.backgroundColor = "	#ff2e2e";
+      document.getElementById([i]).className += " scroll";
+    }
+    if (parseInt(timeBlocks[i].id) > curentHour) {
+      document.getElementById([i]).style.backgroundColor = "#90EE90"
+    }  
+  }
+}
+
 getToDos()
 
 //prints saved todos to correct timeblock
@@ -48,53 +96,9 @@ if (currentMonth ==12 || currentMonth <=2) {
   body.css("background-image", 'url("https://images.pexels.com/photos/235621/pexels-photo-235621.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")')
   }
 
-//function to determine what color a time_block should be
-function whatColor(){
-  for (i = 0; i < timeBlocks.length; i++) {
-    //console.log(timeBlocks[i].id, curentHour)
-    if (parseInt(timeBlocks[i].id) == curentHour) {
-      document.getElementById([i]).style.backgroundColor = "	#ff2e2e";
-      document.getElementById([i]).className += " scroll";
-    }
-    if (parseInt(timeBlocks[i].id) > curentHour) {
-      document.getElementById([i]).style.backgroundColor = "#90EE90"
-    }  
-  }
-}
 
-//scrolls to current time_block hour
-//fix where it scrolls to
-$(document).ready(function () {
-  $('html').animate({
-      scrollTop: $('.scroll').offset().top - 300
-  }, 1200);
-});
 
-//save button
-$('button').on("click", function(){
-  var text = $(this).siblings("textarea").val()
-  var id = $(this).parent().attr('id')
-  var aToDo = new Object()
-  aToDo.id = id
-  aToDo.text = text
-  toDos.push(aToDo)
-  saveText()
-})
 
-//saves to local storage 
-function saveText() {
-  var str = JSON.stringify(toDos)
-  localStorage.setItem("textContent", str)
-}
-
-//gets textContent from local storage 
-function getToDos() {
- var str = localStorage.getItem('textContent')
- toDos = JSON.parse(str)
- if (!toDos) {
-   toDos = []
- }
-}
 
 
 
